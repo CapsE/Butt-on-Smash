c284d907f928bf0bab9a7860231fe719d0a51f8b
Christoph Grensemann <zugiif@googlemail.com>
2015-11-06 22:31:01 +0100

    Button und Pipeline anfang
    Button und so

---
+++ Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonBehavior : MonoBehaviour {
+    public Sprite graphicstype;
+    public string buchstabe;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.Translate(Vector3.down * speed);
+	}
+}
---
+++ Assets/Scripts/PipelineBehavior.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PipelineBehavior : MonoBehaviour {
+    public float intervall;
+    public GameObject[] buttons;
+    private float time;
+    public float spurbreite;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        time += Time.deltaTime;
+        if (time >= intervall) {
+            time = 0;
+            int buttonnr = Random.Range(0, 4);
+            GameObject button = Instantiate(buttons[buttonnr]) as GameObject;
+            button.transform.parent = this.transform;
+            button.transform.localPosition = new Vector3(spurbreite * buttonnr, 0, 0);
+        }
+	}
+}

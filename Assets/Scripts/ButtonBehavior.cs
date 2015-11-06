642824301508e5ec88dbadf9fd2057a3a9b4d718
Christoph Grensemann <zugiif@googlemail.com>
2015-11-07 00:03:06 +0100

    Viel gemacht

--- Assets/Scripts/ButtonBehavior.cs
+++ Assets/Scripts/ButtonBehavior.cs
@@ -14,5 +14,8 @@ public class ButtonBehavior : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
         transform.Translate(Vector3.down * speed);
+        if (transform.position.y <= -4.7) {
+            Destroy(gameObject);
+        }
 	}
 }
--- Assets/Scripts/PipelineBehavior.cs
+++ Assets/Scripts/PipelineBehavior.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PipelineBehavior : MonoBehaviour {
     public float intervall;
     public GameObject[] buttons;
     private float time;
     public float spurbreite;
+    public List<GameObject> activeButtons;
 
 	// Use this for initialization
 	void Start () {
@@ -19,8 +21,34 @@ public class PipelineBehavior : MonoBehaviour {
             time = 0;
             int buttonnr = Random.Range(0, 4);
             GameObject button = Instantiate(buttons[buttonnr]) as GameObject;
+            activeButtons.Add(button);
             button.transform.parent = this.transform;
             button.transform.localPosition = new Vector3(spurbreite * buttonnr, 0, 0);
         }
 	}
+
+    void Update() {
+        if (Input.GetKeyDown("joystick 1 button 0"))    //A
+        {
+            foreach(GameObject c in activeButtons){
+
+            }
+        }
+        if (Input.GetKeyDown("joystick 1 button 1"))    //B
+        {
+            
+        }
+        if (Input.GetKeyDown("joystick 1 button 2"))    //X
+        {
+            
+        }
+        if (Input.GetKeyDown("joystick 1 button 3"))    //Y
+        {
+            
+        }
+        if (Input.GetKeyDown("joystick 2 button 0"))
+        {
+            Debug.Log("joy 2 but 0");
+        }
+    }
 }

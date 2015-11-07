11cd834f190ed98eea919b8af5e555049459b3b3
CapsE <larskrafft@yahoo.de>
2015-11-07 14:13:04 +0100

    Rain Effect geadded

---
+++ Assets/Scripts/Juice/RainBehaviour.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RainBehaviour : MonoBehaviour {
+	
+	public float intervall = 0.15f;
+	public GameObject[] rain0;
+	public GameObject[] rain1;
+
+	public GameObject lightningEffect;
+	public GameObject thunderEffect;
+	public float lightningChanceLow = 1;
+	public float lightningChanceHigh = 5;
+
+	private float lightningChance;
+
+	private float time = -1;
+	private int currentArray = 0;
+	private int current = 0;
+	private GameObject[][] rain;
+
+	// Use this for initialization
+	void Start () {
+		time = Time.realtimeSinceStartup;
+		lightningChance = lightningChanceLow;
+		rain = new GameObject[2][];
+		rain [0] = rain0;
+		rain [1] = rain1;
+		GameController.SkullMoved += SkullMoved;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (time != -1 && Time.realtimeSinceStartup - time >= intervall) {
+			Switch();
+			time = Time.realtimeSinceStartup;
+		}
+		if (Input.GetKeyDown (KeyCode.A)) {
+			SkullMoved(0.7f);
+		}
+		if (Input.GetKeyDown (KeyCode.D)) {
+			SkullMoved(0.2f);
+		}
+	}
+
+	private void SkullMoved(float value){
+		value = Mathf.Abs (value);
+		if (value >= 0.5f) {
+			currentArray = 1;
+			lightningChance = lightningChanceHigh;
+			gameObject.GetComponent<AudioSource>().pitch = 1.5f;
+			foreach(GameObject go in rain[0]){
+				go.SetActive(false);
+			}
+		} else {
+			currentArray = 0;
+			lightningChance = lightningChanceLow;
+			gameObject.GetComponent<AudioSource>().pitch = 1.0f;
+			foreach(GameObject go in rain[1]){
+				go.SetActive(false);
+			}
+		}
+	}
+
+	private void Switch(){
+		current++;
+		if (current >= rain[currentArray].Length) {
+			current = 0;
+		}
+		for(int i = 0; i < rain[currentArray].Length; i++) {
+			if(i == current){
+				rain[currentArray][i].SetActive(true);
+			}else{
+				rain[currentArray][i].SetActive(false);
+			}
+		}
+		if (Random.Range (0.0f, 100.0f) <= lightningChance) {
+			Effect e = new Effect(transform, lightningEffect, 0.3f);
+			Effect t = new Effect(transform, thunderEffect, 2.3f);
+		}
+	}
+}
---
+++ Assets/Scripts/Juice/ThunderBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThunderBehaviour : MonoBehaviour {
+
+	public AudioClip[] thunders;
+
+	// Use this for initialization
+	void Start () {
+		AudioSource audio = gameObject.GetComponent<AudioSource> ();
+		audio.clip = thunders [Random.Range (0, thunders.Length)];
+		audio.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

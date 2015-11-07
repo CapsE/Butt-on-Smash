2ed52e1dd45536f0506c927908fc5ff3936ddd86
Christoph Grensemann <zugiif@googlemail.com>
2015-11-07 17:58:22 +0100

    wichtiges zeug

---
+++ Assets/Scripts/HitboxBehavior.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitboxBehavior : MonoBehaviour {
+    public int position;
+    public int topf; //0 ist links, 1 ist rechts
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (topf == 0)
+        {
+            if (coll.gameObject.name == "player2wurf")
+            {
+                Debug.Log("Kollision an Topf Links " + position + " mit level " + coll.gameObject.GetComponent<ItemBehaviour>().level);
+                Destroy(coll.gameObject); //TODO effekt?
+            }
+        }
+        else {
+            if (coll.gameObject.name == "player1wurf")
+            {
+                Debug.Log("Kollision an Topf Rechts " + position + " mit level " + coll.gameObject.GetComponent<ItemBehaviour>().level);
+                Destroy(coll.gameObject); //TODO effekt?
+            }
+        }
+    }
+}

d32ec9dfd8bdba4f03d9ca601a48c7c46906fd25
CapsE <larskrafft@yahoo.de>
2015-11-07 10:36:10 +0100

    Effect fix

--- Assets/Scripts/Juice/Effect.cs
+++ Assets/Scripts/Juice/Effect.cs
@@ -5,7 +5,6 @@ public class Effect{
 
 	public Effect(Transform parent, GameObject effect, float delay){
 		GameObject go =  GameObject.Instantiate(effect) as GameObject;
-		go.transform.parent = parent;
 		go.transform.position = parent.position;
 		EffectBehaviour eb = go.AddComponent<EffectBehaviour> ();
 		eb.SetDelay (delay);
